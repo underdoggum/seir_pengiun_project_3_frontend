f82d5313881929317aeb9a5451fee76bf0f50b0f
Lucy Liu <lucy.liu94@gmail.com>
2021-11-17 20:25:31 -0500

    added authentication frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Form from './components/SignUpPages/Form';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Form/>
       <Main />
       <Footer />
     </div>
