664682a6ea2914acb982a19354a7b921cbb1ed81
Nathan Noack <nnoack3@gmail.com>
2021-11-18 13:47:14 -0500

    more work on edititem

--- src/pages/EditItem.jsx
+++ src/pages/EditItem.jsx
@@ -1,9 +1,53 @@
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const EditItem = (props) => {
 
+    const [editForm, setEditForm] = useState({
+        name: "",
+        price: 0,
+        quantity: 0,
+        description: "",
+        img: ""
+    })
+
+    const handleChange = (event) => {
+        setEditForm({ ...editForm, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.editItem(editForm)
+        setEditForm({
+            name: "",
+            price: 0,
+            quantity: 0,
+            description: "",
+            img: ""
+        })
+    }
+
+    // const form = (
+    //     <form onSubmit={handleSubmit}>
+    //         <label htmlFor="Name">
+    //             Name: 
+    //             <input
+    //                 type="text"
+    //                 value={editForm.name}
+    //                 name="name"
+    //                 placeholder="Name of Item"
+    //                 onChange={handleChange}
+    //                 required
+    //             />
+    //         </label>
+
     return (
-        <h1>Edit Item</h1>
+        <div className="EditItem">
+            <Link to="/items">
+                <button>Back to Your Items</button>
+            </Link>
+            {/* {form} */}
+        </div>
     )
 }
 
