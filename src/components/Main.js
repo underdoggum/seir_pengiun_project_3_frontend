6c4991b4918d7f400087da8e6fded487c95a5789
Jameson Wang <jamesonw10@gmail.com>
2021-11-17 19:38:41 -0500

    frontend updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,40 @@
+import {useEffect, useState} from "react"
+import {Route, Routes} from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
 const Main = (props) => {
-    return <h1>Main</h1>
+    const [items, setItems] = useState(null)
+
+    const URL = "https://unwasted-backend.herokuapp.com/items/"
+
+    const getItems = async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setItems(data)
+    }
+
+    const createItems = async (item) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+                body: JSON.stringify(item)
+        })
+        getItems()
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    return <main>
+        <Routes>
+            <Route path="/" element={<index/>}/>
+            <Route path="/items/:id" element={<Show/>}/>
+        </Routes>
+    </main>
 }
 
 export default Main;
